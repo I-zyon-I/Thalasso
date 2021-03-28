b8d38138970b971d1a7551f29fc68c3f34053be5
I-zyon-I <81296195+I-zyon-I@users.noreply.github.com>
2021-03-28 15:11:25 +0200

    Add files via upload

--- views/includes/main_home.php
+++ views/includes/main_home.php
@@ -1,21 +1,11 @@
-<main role="main" class="container">
-    <div class="starter-tempalte">
-        <h1 class="text-center">Home Page</h1>
-    </div>
-    <div class="list-group">
-        <article class="list-group-item">
-            <div class="row">
-                <div class="col-12 col-md-4">
-                    <img src="assets/imgs/photo.webp" alt="" class="img-fluid img-thumbnail">
-                </div>
-                <div class="col-12 col-md-8">
-                    <h1>Lorem ipsum dolor sit amet</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam unde quae illo officia
-                        corrupti dicta nihil assumenda cumque, facilis odio porro nemo voluptas, quis quisquam vel
-                        facere tenetur ratione nisi.
-                    </p>
-                </div>
-            </div>
-        </article>
-    </div>
+<main role="main" class="container-fluid">
+    <header>
+        
+    
+    </header>
+    <article>
+    
+    
+    </article>
+    
 </main>
