5ae46070dccacf392d5a1e225c34d0300a8d0b2e
I-zyon-I <81296195+I-zyon-I@users.noreply.github.com>
2021-04-02 14:22:10 +0200

    Add files via upload

---
+++ _classes/entity/Admin.php
@@ -0,0 +1,67 @@
+<?php 
+
+class Admin
+{
+    // Attributs
+    protected $idAdmin;
+    protected $loginAdmin;
+    protected $passwordAdmin;
+
+    // Constructeur
+    public function __construct(string $loginAdmin, string $passwordAdmin)
+    {
+        $this->loginAdmin = $loginAdmin;
+        $this->passwordAdmin = $passwordAdmin;
+    }
+
+    
+
+    /**
+     * Get the value of idAdmin
+     */ 
+    public function getIdAdmin()
+    {
+        return $this->idAdmin;
+    }
+
+    /**
+     * Get the value of loginAdmin
+     */ 
+    public function getLoginAdmin()
+    {
+        return $this->loginAdmin;
+    }
+
+    /**
+     * Set the value of loginAdmin
+     *
+     * @return  self
+     */ 
+    public function setLoginAdmin($loginAdmin)
+    {
+        $this->loginAdmin = $loginAdmin;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of passwordAdmin
+     */ 
+    public function getPasswordAdmin()
+    {
+        return $this->passwordAdmin;
+    }
+
+    /**
+     * Set the value of passwordAdmin
+     *
+     * @return  self
+     */ 
+    public function setPasswordAdmin($passwordAdmin)
+    {
+        $this->passwordAdmin = $passwordAdmin;
+
+        return $this;
+    }
+}
+?>
