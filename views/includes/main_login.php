8305f43a054d113b23840399efb269deb6d06390
I-zyon-I <81296195+I-zyon-I@users.noreply.github.com>
2021-04-02 14:26:48 +0200

    Add files via upload

--- views/includes/main_login.php
+++ views/includes/main_login.php
@@ -1,18 +1,19 @@
 <header>
     
     <div class="header-login g-0 justify-content-center">
-        <div class="login-text col-5 offset-6">
+        <div class="login-text col-5 offset-6 text-center">
             <img src="../../assets/imgs/logo.svg" alt="logo">
-            <h1 class="text-center">Lorem Ipsum</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-                Autem nulla sapiente excepturi accusamus ex inventore saepe illo maxime,
-                non incidunt corrupti mollitia voluptatibus, ea aliquam, quo repellat unde quis. Repellat!
+        </div>
+        <div class="col-3 offset-7">
+            <h1 class="text-center">Connexion à votre compte</h1>
+            <p>Afin d'accéder à votre compte personnel administrateur, veuillez renseigner votre Identifiant et votre Mot de passe.
             </p>
-            <form class="p-2" action="?page=listeSejours" method="post">
-                Identifiant : <input type="text" name="" value="" class="form-control mb-2">
-                Mot de passe : <input type="text" name="" value="" class="form-control mb-2">
+            
+            <form action="?page=listeSejours" method="post">
+                Identifiant : <input type="text" name="loginAdmin" value="" class="form-control mb-2">
+                Mot de passe : <input type="text" name="passwordAdmin" value="" class="form-control mb-2">
                 <input type="submit" name="submitLogin" class="btn button mt-3" value="Connexion">
             </form>
-        </div>
+        </div> 
     </div>
 </header>
